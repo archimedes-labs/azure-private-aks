32a2456bb909c8206e2a870b3ea78a51a46625a6
James Gibbings <jg185270@ncr.com>
2022-10-31 10:43:48 -0400

    Updated app conf client to use azure credential identity

--- blue-green-api/blue-green/bin/bluegreen.js
+++ blue-green-api/blue-green/bin/bluegreen.js
@@ -2,7 +2,8 @@ const azureIdentity = require("@azure/identity");
 const appConfig = require("@azure/app-configuration");
 const credential = new azureIdentity.DefaultAzureCredential();
 const client = new appConfig.AppConfigurationClient(
-    "Endpoint=https://demo-app-conf.azconfig.io;Id=4YPx-l0-s0:I82NKQ00OZ/II7GCX0YA;Secret=MoiPcB4WuvxqOWNBPuAy/VLQpY/nzRMmUz3NnTZo+Nw="
+    "https://demo-app-conf.azconfig.io",
+    credential
 );
 
 async function getConfiguration() {
